8c3e22461adc260a28dcffaac4d8ec74e392070c
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-03-05 22:25:02 -0600

    created and ran tests for each shape

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const shapes = require("./lib/shapes");
 
 inquirer
   .prompt([
@@ -29,19 +30,19 @@ inquirer
   .then((response) => {
     let icon;
     if (response.logoShape === "circle") {
-      icon = new Circle(
+      icon = new shapes.Circle(
         response.logoText,
         response.shapeColor,
         response.textColor
       );
     } else if (response.logoShape === "square") {
-      icon = new Square(
+      icon = new shapes.Square(
         response.logoText,
         response.shapeColor,
         response.textColor
       );
     } else if (response.logoShape === "triangle") {
-      icon = new Triangle(
+      icon = new shapes.Triangle(
         response.logoText,
         response.shapeColor,
         response.textColor
@@ -52,61 +53,3 @@ inquirer
       err ? console.log(err) : console.log("Generated logo.svg")
     );
   });
-
-class Circle {
-  constructor(logoText, shapeColor, textColor) {
-    this.logoText = logoText;
-    this.shapeColor = shapeColor;
-    this.textColor = textColor;
-  }
-
-  render() {
-    return `<svg version="1.1"
-    width="300" height="200"
-    xmlns="http://www.w3.org/2000/svg">
-       <circle cx="150" cy="125" r="80" fill="${this.shapeColor}" />
-     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.logoText}</text>
-
-     </svg>
-`;
-  }
-}
-
-class Square {
-  constructor(logoText, shapeColor, textColor) {
-    this.logoText = logoText;
-    this.shapeColor = shapeColor;
-    this.textColor = textColor;
-  }
-
-  render() {
-    return `<svg version="1.1"
-    width="300" height="200"
-    xmlns="http://www.w3.org/2000/svg">
-    <rect x="62.5" y="10" width="175" height="175" fill="${this.shapeColor}"/>
-     <text x="150" y="115" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.logoText}</text>
-
-     </svg>
-`;
-  }
-}
-
-class Triangle {
-  constructor(logoText, shapeColor, textColor) {
-    this.logoText = logoText;
-    this.shapeColor = shapeColor;
-    this.textColor = textColor;
-  }
-
-  render() {
-    return `<svg version="1.1"
-    width="300" height="200"
-    xmlns="http://www.w3.org/2000/svg">
-    <polygon points="150 5 5 195 295 195"
-    fill="${this.shapeColor}"/>
-     <text x="150" y="160" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.logoText}</text>
-
-     </svg>
-`;
-  }
-}
